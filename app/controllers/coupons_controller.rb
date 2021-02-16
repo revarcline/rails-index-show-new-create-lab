79039493f355246ae9ac4a80b6fc567c4daaed17
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-16 08:08:41 -0600

    commit tree 6f3d25bae3a0ae4ac37b7463a533baa8a3eb62e1

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,25 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    set_coupon
+  end
+
+  def new; end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+  private
+
+  def set_coupon
+    @coupon = Coupon.find(params[:id])
+  end
+end
