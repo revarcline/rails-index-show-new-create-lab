ec376750de5484593bfe2c51b900f1885e899362
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-16 08:08:46 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Coupon.create(coupon_code: '42069', store: 'Peoria 02')
+Coupon.create(coupon_code: '666FFF', store: 'Butzville 23')
+Coupon.create(coupon_code: 'ABBUS', store: 'Beepo 12')
